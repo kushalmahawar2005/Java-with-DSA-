7e7bec38d7562223c3a2efc6a291ec633724200b
kushal <kushalmahawar71@gmail.com>
2025-08-30 00:08:24 +0530

    Added USER INPUT IN SubArray

--- SubArray.java
+++ SubArray.java
@@ -1,16 +1,20 @@
+
+import java.util.*;
+
 public class SubArray {
+
     public static void SubArry(int number[]) {
         int total = 0;
         int minSum = Integer.MAX_VALUE;
         int maxSum = Integer.MIN_VALUE;
-        for (int i = 0 ; i < number.length; i++) {
+        for (int i = 0; i < number.length; i++) {
             int start = i;
-            for (int j = i; j < number.length ; j++) {
+            for (int j = i; j < number.length; j++) {
                 int end = j;
                 int sum = 0;
-                for (int k = start ; k <= end; k++) {
-                    System.out.print(number[k] + " " );
-                    sum += number[k];  
+                for (int k = start; k <= end; k++) {
+                    System.out.print(number[k] + " ");
+                    sum += number[k];
                 }
 
                 if (sum < minSum) {
@@ -20,19 +24,28 @@ public class SubArray {
                 if (sum > maxSum) {
                     maxSum = sum;
                 }
-                
+
                 total++;
                 System.out.println();
             }
             System.out.println();
         }
-        System.out.print("Total Sub Array is :" + total);
+        System.out.println("Total Sub Array is :" + total);
         System.out.println("Minimum Subarray :" + minSum);
-        System.out.println("Maximum Subarray : " +  maxSum);
-        
+        System.out.println("Maximum Subarray : " + maxSum);
+
     }
-    public static void main (String[] args) {
-        int number[] = {2 , 4 , 6 , 8 , 10};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Size of array");
+        int n = sc.nextInt();
+
+        int number[] = new int[n];
+        System.out.print("Enter the Element in the array :");
+        for (int i = 0; i < n; i++) {
+            number[i] =  sc.nextInt();
+        }
         SubArry(number);
     }
 }
