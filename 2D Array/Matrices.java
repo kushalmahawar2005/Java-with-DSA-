884fcd5293ecd01f3a98bb3506b1bac2e9eafe8f
kushal <kushalmahawar71@gmail.com>
2025-09-08 22:31:27 +0530

    2D array and Find the key Element in 2D array

---
+++ 2D Array/Matrices.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+
+public class Matrices {
+
+    public static boolean search(int matrix[][], int key) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.println("Found at cell (" + i + ", " + j + ")");
+                }
+            }
+        }
+        System.out.println("Key Not Found");
+        return false;
+    }
+
+    public static void main(String args[]) {
+        int matrix[][] = new int[3][3];
+        int n = matrix.length, m = matrix[0].length;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Fill the 2D Array" + "");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        //for Output
+        System.out.println("Output is :" + " ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        search(matrix, 5);
+    }
+}
+ 
