4202baae3a4c03a6eb70f7e5f646fb216504e2c9
kushal <kushalmahawar71@gmail.com>
2025-09-09 23:31:54 +0530

    Digonal Matrix and staircaseSearch

---
+++ 2D Array/DigonalSum.java
@@ -0,0 +1,42 @@
+
+public class DigonalSum {
+
+    public static int DigonalSum(int matrix[][]) {
+        int sum = 0;
+        //     for(int i = 0; i < matrix.length; i++) {
+        //         for (int j = 0; j < matrix[0].length; j++) {
+        //             if(i == j) {
+        //                 sum += matrix[i][j];
+        //             }
+        //             else if(i + j == matrix.length - 1) {
+        //                 sum += matrix[i][j];  
+        //             }
+        //         }
+
+        //     }
+        //     System.out.println(sum);
+        //     return sum;
+        // }
+        for (int i = 0; i < matrix.length; i++) {
+            //pd
+            sum += matrix[i][i];
+            //sd
+            if (i != matrix.length - 1 - i) {
+                sum += matrix[i][matrix.length - i - 1];
+            }
+        } 
+        System.out.println(sum);
+        return sum;
+        
+    }
+
+    public static void main(String args[]) {
+        int matrix[][] = {{1, 2, 3, 4},
+        {5, 6, 7, 8,},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}};
+
+        int result = DigonalSum(matrix);
+
+    }
+}
