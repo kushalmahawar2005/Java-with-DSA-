f37cc6995052569a6165205b3b8e65931a9bd48a
kushal <kushalmahawar71@gmail.com>
2025-09-10 23:06:02 +0530

    Palindrom

---
+++ String/Palindrom.java
@@ -0,0 +1,22 @@
+
+public class Palindrom {
+
+    public static boolean Palindrom(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            int n = str.length();
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                //not a palindrom 
+                return false;
+            }
+
+            
+        } return true;
+
+    }
+
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(Palindrom(str));
+
+    }
+}
