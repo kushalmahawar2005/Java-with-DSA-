8933108e10758e7c40ae66a2ef5d4862649d258b
kushal <kushalmahawar71@gmail.com>
2025-10-02 23:48:33 +0530

    fiboonaci via recursion

---
+++ Recurrsion/fabonaci.java
@@ -0,0 +1,18 @@
+
+public class fabonaci {
+
+    public static int fib(int n) {
+        if ( n == 0 || n == 1) {
+            return n;
+        }
+        int fn1 = fib(n - 1);
+        int fn2 = fib(n - 2);
+        int fn = fn1 + fn2;
+        return fn ;
+    }
+   
+    public static void main(String args[]) {
+        int n = 5;
+      System.out.println(fib(n));
+    }
+}
