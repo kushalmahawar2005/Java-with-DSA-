a37d10433aa95b4d145d8557cec9b8604153bf4e
kushal <kushalmahawar71@gmail.com>
2025-12-28 23:06:28 +0530

    N_Queen Problem

---
+++ BackTracing/N_Queen.java
@@ -0,0 +1,20 @@
+
+public class N_Queen {
+
+    public static void main(String args[]) {
+        int n = 2;
+        char Board[][] = new char[n][n];
+        //initiallization 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+    }
+    
+}
+     
+    
+        
+
+     
