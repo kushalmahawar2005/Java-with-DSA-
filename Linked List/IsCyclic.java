086da1909844fc7ca884efc31b1f6a73b34dc4ef
kushalmahawar2005 <kushalmahawar71@gmail.com>
2026-02-12 22:35:00 +0530

    To check that linked list isCyclic or not

---
+++ Linked List/IsCyclic.java
@@ -0,0 +1,35 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class IsCyclic {
+
+    public static boolean isCyclic(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        // head.next.next.next = head;
+        System.out.println(isCyclic(head));
+    }
+}
